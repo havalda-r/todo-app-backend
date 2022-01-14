c09d948f482a14b5c9ed00e6398f2cef92ac3d2c
Richard Havalda <havalda.r94@gmail.com>
2022-01-14 21:40:24 +0100

    feat: test path variable

--- src/main/java/com/rich/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/rich/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.rich.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +12,15 @@ public class HelloWorldController {
         return "Hello World";
     }
 
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello, %s", name));
+    }
+
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
+
+
 }
