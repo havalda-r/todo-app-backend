4a59b4a3caea354e629c670dc41ae7f0bb229442
Richard Havalda <havalda.r94@gmail.com>
2022-01-14 21:02:30 +0100

    feat: initialize spring boot project

---
+++ src/test/java/com/rich/rest/webservices/restfulwebservices/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rich.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
