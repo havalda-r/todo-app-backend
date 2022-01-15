a0a05e2b3bc39bcc571d1a90f2f5c190ec99a801
Richard Havalda <havalda.r94@gmail.com>
2022-01-15 18:52:55 +0100

    feat: add post route

--- src/main/java/com/rich/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ src/main/java/com/rich/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,9 +1,12 @@
 package com.rich.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,5 +31,18 @@ public class TodoResource {
         Todo todo = todoService.deleteById(id);
         if (todo != null) return ResponseEntity.noContent().build();
         return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("users/{username}/todos/{id}")
+    public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
+        Todo todoUpdated = todoService.save(todo);
+        return new ResponseEntity<Todo>(todo, HttpStatus.OK);
+    }
+
+    @PostMapping("users/{username}/todos")
+    public ResponseEntity<Void> addTodo(@PathVariable String username, @RequestBody Todo todo) {
+        Todo createdTodo = todoService.save(todo);
+        URI uri  = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
