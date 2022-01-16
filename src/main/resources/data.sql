cb07ef45e7cec70953c1f63d8a55a760336d48af
Richard Havalda <havalda.r94@gmail.com>
2022-01-16 20:01:15 +0100

    feat: switch to postgres

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into todo (id, description, target_date, username,is_done) values ( 10001,'Learn React', sysdate(),'in28minutes',false );
-insert into todo (id, description, target_date, username,is_done) values ( 10002,'Learn Angular', sysdate(),'in28minutes',false );
-
--- src/main/resources/schema.sql
+++
@@ -1,2 +0,0 @@
-drop table if exists todo;
-create table todo (id bigint not null, description varchar(255), is_done boolean not null, target_date timestamp, username varchar(255), primary key (id));
